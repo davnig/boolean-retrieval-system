7e20c27728a2e26aabc414ead7a67bd6b629e7ab
davnig <daddets97@gmail.com>
2023-01-03 15:23:36 +0100

    refactor: rename main class

--- src/main/java/com/davnig/units/BooleanRetrievalSystem.java
+++ src/main/java/com/davnig/units/BooleanRetrievalSystem.java
@@ -4,7 +4,7 @@ import com.davnig.units.model.Corpus;
 import com.davnig.units.model.Movie;
 import com.davnig.units.model.MoviePositionalIndex;
 
-public class Main {
+public class BooleanRetrievalSystem {
 
     public static void main(String[] args) {
         CorpusReader<Movie> corpusReader = new MovieCorpusReader();
