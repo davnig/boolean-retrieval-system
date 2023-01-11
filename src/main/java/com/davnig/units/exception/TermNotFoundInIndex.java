c8d633599fda81c69d2816f00d67f272618872a5
davnig <daddets97@gmail.com>
2023-01-11 12:09:14 +0100

    chore: remove unused exception class

--- src/main/java/com/davnig/units/exception/TermNotFoundInIndex.java
+++
@@ -1,9 +0,0 @@
-package com.davnig.units.exception;
-
-public class TermNotFoundInIndex extends RuntimeException {
-
-    public TermNotFoundInIndex(String term) {
-        System.err.printf("The term %s was not found in index%n", term);
-    }
-
-}
